9ba0591fc3ae8f2daef213575cb9dc7e66e4d9a3
G-jjunny <rudwns9551@naver.com>
2024-09-23 16:32:54 +0900

    api호출 코드 변경

--- FE/src/pages/airlinePage/airlineSearchComponents/api/fetchAirlineData.ts
+++ FE/src/pages/airlinePage/airlineSearchComponents/api/fetchAirlineData.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { searchResultType } from "../searchResultType";
 
-const AIRLINE_KEY = process.env.REACT_APP_AIRLINE_SERVICE_KEY;
+const AIRLINE_KEY = decodeURIComponent(
+  process.env.REACT_APP_AIRPORT_SERVICE_KEY!
+); // key자체가 이미 인코딩이 되어있는 값이라 한 번 디코딩
 const AIRLINE_URL = process.env.REACT_APP_AIRLINE_SERVICE_URL;
 
 const ARRIVAL_ENDPOINT = process.env.REACT_APP_AIRLINE_ARRIVAL_ENDPOINT;
@@ -10,10 +12,11 @@ const DEPARTURE_ENDPOINT = process.env.REACT_APP_AIRLINE_DEPARTURE_ENDPOINT;
 /** endpoint에 따른 fetch 요청 함수 */
 /** 나중에 커스텀 훅으로 빼기 */
 const fetchByEndpoint = async (endpoint: string, params: any) => {
+  // console.log("Full URL:", `${AIRLINE_URL}${endpoint}`);
   return axios.get(`${AIRLINE_URL}${endpoint}`, {
     params: {
       ...params,
-      serviceKey: AIRLINE_KEY,
+      serviceKey: AIRLINE_KEY, // 이미 인코딩된 키를 다시 인코딩하지 않도록 처리
       type: "json",
     },
   });
